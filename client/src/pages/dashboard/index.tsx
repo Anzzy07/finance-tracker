ead4bd4325ff40e65c71f1136aac354e6870d094
Anzel <97039860+Anzzy07@users.noreply.github.com>
2024-11-28 13:35:26 +0545

    setup

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,3 +1,14 @@
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./Financial-record-list";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user } = useUser();
+  return (
+    <div className="dashboard-container">
+      <h1>Welcome {user?.firstName}! Here Are Your Finances</h1>
+      <FinancialRecordForm />
+      <FinancialRecordList />
+    </div>
+  );
 };
---
+++ server/src/index.ts
@@ -0,0 +1,1 @@
+// password : 08vWXvSHRYZkSiFL user: anzel
