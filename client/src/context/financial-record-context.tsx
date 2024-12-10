9bc0932e297fd91de4fac583301a7ec9f54d5ad1
Anzel <97039860+Anzzy07@users.noreply.github.com>
2024-12-10 18:24:43 +0545

    table

--- client/src/context/financial-record-context.tsx
+++ client/src/context/financial-record-context.tsx
@@ -1,8 +1,10 @@
+import { useUser } from "@clerk/clerk-react";
 import {
   Children,
   createContext,
   ReactNode,
   useContext,
+  useEffect,
   useState,
 } from "react";
 
@@ -33,6 +35,23 @@ export const FinacialRecordsProvider = ({
   children: ReactNode;
 }) => {
   const [records, setRecords] = useState<FinancialRecord[]>([]);
+  const { user } = useUser();
+
+  const fetchRecords = async () => {
+    if (!user) return;
+    const response = await fetch(
+      `http://localhost:3001/financial-records/getAllByUserID/${user.id}`
+    );
+
+    if (response.ok) {
+      const records = await response.json();
+      setRecords(records);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, [user]);
 
   const addRecord = async (record: FinancialRecord) => {
     const response = await fetch("http://localhost:3001/financial-records", {
--- client/src/pages/dashboard/Financial-record-list.tsx
+++ client/src/pages/dashboard/Financial-record-list.tsx
@@ -1,3 +1,17 @@
+import { useFinacialRecords } from "../../context/financial-record-context";
+import { useTable, Column, CellProps, Row } from "react-table";
+
 export const FinancialRecordList = () => {
-  return <div>Financial-record-list</div>;
+  const { records } = useFinacialRecords();
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: records });
+  return (
+    <div className="table-container">
+      <table {...getTableBodyProps()} className="table">
+        <thead>{headerGroups}</thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 };
